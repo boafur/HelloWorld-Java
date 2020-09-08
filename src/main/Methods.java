91b5fe7a165d84838112fda0b07a6a47af4b88ee
Boa <rubberboa01@gmail.com>
2020-09-07 19:56:20 -0700

    Update Methods.java

--- src/main/Methods.java
+++ src/main/Methods.java
@@ -2,14 +2,6 @@ package main;
 
 public class Methods {
 	
-	public String removeLastChar(String str) {
-        return removeLastChars(str, 1);
-    }
-
-    public String removeLastChars(String str, int chars) {
-        return str.substring(0, str.length() - chars);
-    }
-	
 	public void wait(int ms) {
 		try {
 			Thread.sleep(ms);
