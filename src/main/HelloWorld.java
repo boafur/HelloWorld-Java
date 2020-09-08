850237b9dda1935de2207d6dd1e051e12bb73d04
boafur <rubberboa01@gmail.com>
2020-09-08 03:20:32 -0700

    e

--- src/main/HelloWorld.java
+++ src/main/HelloWorld.java
@@ -6,16 +6,18 @@ public class HelloWorld {
 		Methods m = new Methods();
 		String message = "Hello world!";
 		char[] chars = message.toCharArray();
-		
-		for (int i = 0; i < chars.length; i++) {
-			System.out.print(chars[i]);
+
+		for (char aChar : chars) {
+			System.out.print(aChar);
 			m.wait(250);
-			
+
 		}
+		m.wait(250);
 		for (int j = 0; j < chars.length; j++) {
 			String bc = "\b \b";
 			System.out.print(bc);
 			m.wait(250);
+			
 		}
 		System.out.println("\uD83C\uDF54");
 		m.wait(25);
